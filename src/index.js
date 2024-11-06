9f10792e034c56b9ff93c4aba00102a4be3c1c88
A.Abad GitHub08 <72219148+Alangithub08@users.noreply.github.com>
2024-11-07 02:54:57 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,25 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import fetch from 'node-fetch';
+
+async function getUser() {
+  try {
+    const response = await fetch('https://randomuser.me/api/');
+
+    if (!response.ok) {
+      throw new Error(`Error! status: ${response.status}`);
+    }
+
+    const result = await response.json();
+    return result;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+console.log(await getUser());
+
+
 // import registerServiceWorker from './registerServiceWorker';
 import 'tachyons';
 
